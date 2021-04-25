598550beffc221e082ffd7270f1ca76d45851d75
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-04-25 23:22:02 +1000

    +home component

---
+++ src/components/home/index.js
@@ -0,0 +1,21 @@
+import logo from '../logo.png';
+import './style.css';
+function Home() {
+    return (
+        <div className="home container-md">
+            <div className="row">
+                <div className="home-left col-xl-4">
+                    <img src={logo} className="logo mb-5" alt="logo" />
+                    <div>
+                        <h1 className="mb-2 mt-5">LOREM IPSUM</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nisl elit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu leo elit. Donec condimentum odio ut ante bibendum, quis egestas quam semper.</p>
+                        <p>Donec tempus cursus magna a feugiat. Morbi scelerisque euismod aliquet. Integer ut euismod risus. Aenean a diam vel nisl accumsan cursus.</p>
+                        <p>Quisque ac dapibus nisl. Etiam rhoncus aliquet ligula ac convallis. Ut eleifend sit amet justo eu laoreet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Home;
