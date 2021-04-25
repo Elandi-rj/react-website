b801ef30564122188d9a68cb1ecfff830b29ac68
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-04-25 15:42:37 +1000

    +Front page sketch

---
+++ src/components/playbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+function Playbar() {
+    return (
+        <div className="bar-container">
+            <button>▶</button>
+        </div>
+    )
+}
+export default Playbar;
