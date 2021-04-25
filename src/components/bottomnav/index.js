dac3ec2d9b0da06448ec1b466583ee36a3140452
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-04-25 23:52:34 +1000

    +bottomnav component

---
+++ src/components/bottomnav/index.js
@@ -0,0 +1,10 @@
+
+import './style.css';
+function Bottomnav() {
+    return (
+        <div className="bottom-nav">
+            hi
+        </div>
+    );
+}
+export default Bottomnav;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,12 @@
 import logo from '../logo.png';
 import './style.css';
+import Bottomnav from '../bottomnav';
 function Home() {
     return (
         <div className="home container-md">
             <div className="row">
                 <div className="home-left col-xl-4">
-                    <img src={logo} className="logo mb-5" alt="logo" />
+                    <img src={logo} className="logo mb-5 col-12" alt="logo" />
                     <div>
                         <h1 className="mb-2 mt-5">LOREM IPSUM</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nisl elit.</p>
@@ -15,6 +16,7 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <Bottomnav />
         </div>
     );
 }
