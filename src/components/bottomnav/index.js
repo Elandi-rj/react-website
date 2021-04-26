4bab7f63cf6497419bf9925ae5f36a7f8e083595
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-04-26 16:52:40 +1000

    fix bottom nav styling
    now only effects bottom nav instead of all NavLinks

--- src/components/bottomnav/index.js
+++ src/components/bottomnav/index.js
@@ -7,18 +7,16 @@ function Bottomnav() {
     return (
         <div className="bottom-nav container-fluid">
             <ul className="row">
-                <li className="col-2 d-flex justify-content-center mb-5">
-                    <NavLink exact activeClassName="active" to="/home">
-                        <FontAwesomeIcon icon={faHome} /> Home
-                    </NavLink>
+                <li className="col-2 d-flex justify-content-center mb-4">
+                    <NavLink exact activeClassName="active" to="/home"><FontAwesomeIcon icon={faHome} /> Home</NavLink>
                 </li>
-                <li className="col-2 d-flex justify-content-center mb-5">
+                <li className="col-2 d-flex justify-content-center mb-4">
                     <NavLink activeClassName="active" to="/topic1">TOPIC 1</NavLink>
                 </li>
-                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 2</li>
-                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 3</li>
-                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 4</li>
-                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 5</li>
+                <li className="dummylink col-2 d-flex justify-content-center mb-4">TOPIC 2</li>
+                <li className="dummylink col-2 d-flex justify-content-center mb-4">TOPIC 3</li>
+                <li className="dummylink col-2 d-flex justify-content-center mb-4">TOPIC 4</li>
+                <li className="dummylink col-2 d-flex justify-content-center mb-4">TOPIC 5</li>
             </ul>
         </div>
     );
