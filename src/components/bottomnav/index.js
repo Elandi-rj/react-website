f16cd204fabc04ae8f1fa865269ec12500d90883
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-04-26 16:24:44 +1000

    styling to fit figma for bottom nav

--- src/components/bottomnav/index.js
+++ src/components/bottomnav/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
 function Bottomnav() {
     return (
-        <div className="bottom-nav">
-            hi
+        <div className="bottom-nav container-fluid">
+            <ul className="row">
+                <li className="col-2 d-flex justify-content-center mb-5">
+                    <NavLink exact activeClassName="active" to="/home">
+                        <FontAwesomeIcon icon={faHome} /> Home
+                    </NavLink>
+                </li>
+                <li className="col-2 d-flex justify-content-center mb-5">
+                    <NavLink activeClassName="active" to="/topic1">TOPIC 1</NavLink>
+                </li>
+                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 2</li>
+                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 3</li>
+                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 4</li>
+                <li className="col-2 d-flex justify-content-center mb-5">TOPIC 5</li>
+            </ul>
         </div>
     );
 }
